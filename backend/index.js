8931c07d6a2dd0cc02741be2ce4e8b3fdad670ed
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-01-03 16:04:05 +0200

    Removed dist

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 // app.use(requestLogger)
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 
 let notes = [
   {
