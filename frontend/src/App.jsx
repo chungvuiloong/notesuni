592abf07b07433e346f97f545efeaf50319422ed
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-01-04 01:53:53 +0200

    Ran deploy from root folder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes NEW TEST TEST TEST TEST</h1>
+      <h1>Notes</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
