29772bb410651093e331a78f16ce1ad8aa4639c4
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2024-01-04 01:27:42 +0200

    Push the dist.

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const unknownEndpoint = (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 let notes = [
   {
